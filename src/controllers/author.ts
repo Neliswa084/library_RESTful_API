86ca1f73d7ffad969cdc5ee7624fb934f88f5250
Neliswa084 <neliswangcobo141@gmail.com>
2026-09-18 13:43:58 +0200

    Complete the Post author end point

--- src/controllers/author.ts
+++ src/controllers/author.ts
@@ -7,6 +7,18 @@ export const getAllAuthors =(req: Request, res:Response) => {
     res.status(200).json(authors)
 }
 
-export const  createNewAuthor = (req: Request , res : Response) =>{
-    
-} 
+
+export const createNewAuthor = (req: Request, res: Response) => {
+    const { firstName, lastName, emailAddress } = req.body
+
+    const newAuthor: Authors = {
+        id: authors.length + 1,
+        firstName,
+        lastName,
+        emailAddress
+    }
+
+    authors.push(newAuthor)
+
+    res.status(201).json(newAuthor)
+}
--- src/routes/author.ts
+++ src/routes/author.ts
@@ -1,10 +1,23 @@
 import {Router ,Request, Response} from "express";
 import {body, param, validationResult} from "express-validator";
-import { getAllAuthors } from "../controllers/author";
+import { getAllAuthors ,createNewAuthor} from "../controllers/author";
 
 const router = Router()
 
 router.get("/", getAllAuthors)
 
+router.post("/",[
+    body("firstName").notEmpty().withMessage("First Name is required"),
+    body("lastName").notEmpty().withMessage("Last Name is required"),
+    body("emailAddress").isEmail().withMessage("Must be a valid email")
+], (req: Request, res: Response) =>{
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array})
 
+    }
+    createNewAuthor(req, res)
+
+}
+)
 export default router;
