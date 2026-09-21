bc6e4e68068521c6fb57a593ddfa372b99fb22e6
Neliswa084 <neliswangcobo141@gmail.com>
2026-09-21 06:29:03 +0200

    Get Book By ID ENd Point

--- src/controllers/book.ts
+++ src/controllers/book.ts
@@ -6,3 +6,13 @@ let books : Books[] =[]
 export const getAllBooks = (req: Request, res: Response) => {
     res.status(200).json(books)
 }
+
+export const getBookById= (req: Request, res: Response) => {
+    const { id } = req.params
+    const book = books.find((book) => book.id === parseInt(id as string))
+
+    if (!book) {
+        return res.status(404).json({ message: "Book not found" })
+    }
+    res.status(200).json(book)
+}
--- src/routes/book.ts
+++ src/routes/book.ts
@@ -1,10 +1,21 @@
 import {Router ,Request, Response} from "express";
 import {body, param, validationResult} from "express-validator";
-import { getAllBooks } from "../controllers/book";
+import { getAllBooks ,  getBookById } from "../controllers/book";
 
 const router = Router()
 
 
 router.get("/", getAllBooks)
+
+router.get("/:id" ,
+    [param("id").isInt().withMessage("Id must be an interger")],
+    (req:Request , res: Response) =>{
+        const errors = validationResult(req)
+        console.log(errors , "Errors from express validator middleware")
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()});
+    }
+    getBookById(req, res)
+})
 
 export default router;
